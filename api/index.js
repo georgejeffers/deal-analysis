af26d3f648065ad8811ac988e56b55f41cc75b40
georgejeffers <georgejefferson2005@gmail.com>
2024-09-26 18:08:00 +0100

    push to prod

--- api/index.js
+++ api/index.js
@@ -13,7 +13,15 @@ export default async function handler(req, res) {
   try {
     const proxiedUrl = `${CORS_PROXY}${url}`;
     console.log('Fetching URL:', proxiedUrl);
-    const response = await axios.get(proxiedUrl);
+    const response = await axios.get(proxiedUrl, {
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        'Connection': 'keep-alive'
+      }
+    });
     console.log('Response received, status:', response.status);
     
     const $ = cheerio.load(response.data);
