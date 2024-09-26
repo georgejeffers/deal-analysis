0fe77e4aa82180a8fd9de72d76928f48f7f939d4
georgejeffers <georgejefferson2005@gmail.com>
2024-09-26 18:01:07 +0100

    push to prod

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios from 'https://cdn.jsdelivr.net/npm/axios@1.3.5/+esm';
 
 // Remove these lines as they're no longer needed
 // const isBrowser = typeof window !== 'undefined' && typeof window.document !== 'undefined';
